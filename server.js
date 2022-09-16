78a1138ad567bdf1da6824224819c6d8636a0b61
Jesse Pence <102560332+jazzypants1989@users.noreply.github.com>
2022-09-15 21:45:50 -0700

    i am a mo frickin monstah

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 2121;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+app.all('*' , (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not found' })
+    } else {
+        res.type('txt').send('404 Not found')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
